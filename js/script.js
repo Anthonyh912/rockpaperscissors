de7f8e3e87c87f25c1adbee12eaf62b242ac5061
Anthony Hernandez <hernandez.aj912@gmail.com>
2019-11-21 23:06:24 +0000

    work in progress

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
 $("#shoot").click(function () {
   var choice = Math.random();
 
-    let answer = $("#input").val()
-    $("#userChoice").html(answer)
+    let userAnswer = $("#input").val();
+    let computerAnswer = "";
+    $("#userChoice").html(userAnswer);
+    
 
   if (choice > .60) {
-    $("#computerChoice").html("Rock");
+    $("#computerChoice").html("rock");
+    computerAnswer="Rock";
   } else if (choice < .60 & choice > .30) {
-    $("#computerChoice").html("Paper");
+    $("#computerChoice").html("paper");
+    computerAnswer="Paper";
   } else {
-    $("#computerChoice").html("Scissors");
+    $("#computerChoice").html("scissors");
+    computerAnswer="Scissors";
   }
+  
+ if (userAnswer === computerAnswer) {
+      $("#result").html("Tie!"); 
+  } else if (userAnswer === "rock" && computerAnswer === "Paper") {
+      $("#result").html("Paper Wins!");
+  } else if (userAnswer === "rock" && computerAnswer === "Scissors") {
+      $("#result").html("Rock Wins!");
+  } else if (userAnswer === "paper" && computerAnswer === "Rock") {
+      $("#result").html("Paper Wins!"); 
+  } 
+
+
+ 
 });
 
